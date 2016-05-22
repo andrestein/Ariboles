fc0678a1d5c7d61551a50e936129b5db0dc16004
andrestein <andresgrandapiedrhaita@gmail.com>
2016-05-21 19:10:32 -0500

    añadiendo clase inicial y clase proceso con metodo para hacer el arbol binario

---
+++ AndresGranda_SmithAgudelo_JuanPalacio/src/arbolesbinarios/Inicial.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package arbolesbinarios;
+
+import java.util.Scanner;
+import static jdk.nashorn.internal.objects.NativeRegExp.source;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class Inicial {
+    
+    public static void main(String args[]) {
+        System.out.println("ingrese la cadena de texto");
+        Scanner sc =new Scanner(System.in);
+        Proceso proceso=new Proceso(sc.nextLine());        
+    }
+}
---
+++ AndresGranda_SmithAgudelo_JuanPalacio/src/arbolesbinarios/Proceso.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package arbolesbinarios;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class Proceso {
+    
+    private String texto;
+    private ABB arbol;
+    
+    public Proceso(String texto){
+        this.texto = texto;
+        arbol = new ABB();
+    }
+    
+    public void crearArbol(){
+        String palabra = "";
+        for(int i = 0;i<texto.length();i++){
+            if(texto.charAt(i) != ' '){
+                palabra = palabra+texto.charAt(i);
+            }else{
+                arbol.add(palabra);
+                palabra="";
+            }
+        }
+    }    
+}
